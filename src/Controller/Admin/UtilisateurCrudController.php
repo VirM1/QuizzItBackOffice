3ed25a6ee72e2687a308373f68cffb1ae9f43fb1
Virgile <virgile.louis.mathieu@gmail.com>
2022-05-18 16:53:05 +0200

    new secret + tests

--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UtilisateurCrudController extends AbstractCrudController
 {
@@ -12,14 +17,23 @@ class UtilisateurCrudController extends AbstractCrudController
         return Utilisateur::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+   public function configureFields(string $pageName): iterable
+   {
+       yield IdField::new("id","id");
+
+       yield EmailField::new("email",'utilisateur.email');
+
+       yield TextField::new("nomUtilisateur","utilisateur.nom");
+
+       yield TextField::new("prenomUtilisateur","utilisateur.prenom");
+
+       yield ChoiceField::new("roles","utilisateur.roles")->setChoices(Utilisateur::AVAILABLE_ROLES);
+   }
+
+   public function configureCrud(Crud $crud): Crud
+   {
+       return $crud
+           ->setEntityLabelInSingular("utilisateur.singular")
+           ->setEntityLabelInPlural("utilisateur.plural");
+   }
 }
